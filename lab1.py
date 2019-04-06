48df1105622197c1f81286914de43bda10d3c859
kaaung <eddiekaung99@gmail.com>
2019-04-06 11:15:39 -0700

    added comments

--- lab1.py
+++ lab1.py
@@ -1,10 +1,9 @@
-
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   if int_list == None:
+   if int_list == None:       # error handling
       raise ValueError
-   elif len(int_list) == 0:
+   elif len(int_list) == 0:   # when the list is empty
       return None
    else:
       max_num = int_list[0]
@@ -17,28 +16,28 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   if int_list == None:
+   if int_list == None:      # error when list is None
       raise ValueError
-   elif len(int_list) == 0:
+   elif len(int_list) == 0:  # when the list is empty
       return []
    else:
       if len(int_list) == 1:
          return [int_list[0]]
-      return [int_list[-1]] + reverse_rec(int_list[:-1])
+      return [int_list[-1]] + reverse_rec(int_list[:-1])  # slicing list
 
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   if int_list == None:
+   if int_list == None:     # error when list is NOne    
       raise ValueError
-   if high < low:
+   if high < low:           # returns None if target is not found
       return None
    midpoint = (high + low) // 2
    if int_list[midpoint] == target:
       return midpoint
-   elif target < int_list[midpoint]:
+   elif target < int_list[midpoint]:   # when target is lower than midpoint
       return bin_search(target, low, midpoint - 1, int_list)
-   elif target > int_list[midpoint]:
+   elif target > int_list[midpoint]:   # when target is greater than midpoint
       return bin_search(target, midpoint + 1, high, int_list)
 
