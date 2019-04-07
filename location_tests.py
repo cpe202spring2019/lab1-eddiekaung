cefd58154f1051dcb2382e3c83a00d8fa5cfce9b
kaaung <eddiekaung99@gmail.com>
2019-04-07 15:38:31 -0700

    added more test cases and comments

--- location_tests.py
+++ location_tests.py
@@ -4,13 +4,29 @@ from location import *
 class TestLab1(unittest.TestCase):
 
     def test_repr(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        """test case for repr method"""
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
+        loc2 = Location("New York", 40.7, 74.0)
+        self.assertEqual(repr(loc2),"Location('New York', 40.7, 74.0)")
+        loc3 = Location("LA", 34.05, 118.24)
+        self.assertEqual(repr(loc3),"Location('LA', 34.05, 118.24)")
     
     # Add more tests!
     def test_eq(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(loc, Location("SLO", 35.3, -120.7))
+        """test case for eq method"""
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(loc1, Location("SLO", 35.3, -120.7))
+        loc2 = Location("New York", 40.71, 74.00)
+        self.assertEqual(loc2, Location("NYC", 40.71, 74.00))
+        loc3 = Location("LA", 34.05, 118.24)
+        self.assertEqual(loc3, Location("Los Angeles", 34.05, 118.24))
+
+    def test_eq_wrong_type(self):
+        """test case for when Location is compared with other type"""
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertFalse(loc1 == "San Luis Obispo")   # expects False
+        
 
 if __name__ == "__main__":
         unittest.main()
