2571c2d7258ae006150eb5ece7930615035f92cc
kaaung <eddiekaung99@gmail.com>
2019-04-10 21:25:14 -0700

    added more test cases for eq function

--- location_tests.py
+++ location_tests.py
@@ -18,14 +18,29 @@ class TestLab1(unittest.TestCase):
         loc1 = Location("SLO", 35.3, -120.7)
         self.assertEqual(loc1, Location("SLO", 35.3, -120.7))
         loc2 = Location("New York", 40.71, 74.00)
-        self.assertEqual(loc2, Location("NYC", 40.71, 74.00))
+        self.assertEqual(loc2, Location("New York", 40.71, 74.00))
         loc3 = Location("LA", 34.05, 118.24)
-        self.assertEqual(loc3, Location("Los Angeles", 34.05, 118.24))
+        self.assertEqual(loc3, Location("LA", 34.05, 118.24))
 
     def test_eq_wrong_type(self):
         """test case for when Location is compared with other type"""
         loc1 = Location("SLO", 35.3, -120.7)
         self.assertFalse(loc1 == "San Luis Obispo")   # expects False
+
+    def test_eq_wrong_name(self):
+        """test case for when lat and lon are the same and name is different"""
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertNotEqual(loc1, Location("San Luis Obispo", 35.3, -120.7))   # expects False
+
+    def test_eq_different_lat(self):
+        """test case for when lon and name are the same and lat is different"""
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertNotEqual(loc1, Location("SLO", 36, -120.7))   # expects False
+
+    def test_eq_different_lon(self):
+        """test case for when lat and name are the same and lon is different"""
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertNotEqual(loc1, Location("SLO", 35.3, -121))   # expects False
         
 
 if __name__ == "__main__":
